65c2035bbd47d567a866c36d8f49aafc8d8a2b12
horisaki <seiji.horisaki@gmail.com>
2018-12-11 18:29:31 +0900

    復習

--- d-Christmas.rb
+++ d-Christmas.rb
@@ -1,21 +1,32 @@
 N, X = gets.chomp.split.map(&:to_i)
-BAN = "B"
-PAT = "P"
 
-def n_1bargar(bargar, num)
-  if num == 0
-    bargar << PAT
+$a, $p = [1], [1]
+
+N.times do |i|
+  $a << $a[i] * 2 + 3
+  $p << $p[i] * 2 + 1
+end
+
+# p $a
+# p $p
+# $count = 0
+def f(n, x)
+  # $count += 1
+  if n == 0
+    x <= 0 ? 0 : 1
+  elsif x <= 1
+    0
+  elsif 1 < x && x <= 1 + $a[n - 1]
+    f(n - 1, x - 1)
+  elsif x == 2 + $a[n - 1]
+    $p[n - 1] + 1
+  elsif 2 + ($a[n - 1]) < x && x <= 2 + 2 * $a[n - 1]
+    $p[n - 1] + 1 + f(n - 1, x - 2 - $a[n - 1])
   else
-    bargar << BAN
-    n_1bargar(bargar, num - 1)
-    bargar << PAT
-    n_1bargar(bargar, num - 1)
-    bargar << BAN
-
+    2 * $p[n - 1] + 1
   end
 end
-bargar = ""
-n_1bargar(bargar, N)
-eat = bargar.slice(0, X)
 
-puts eat.count(PAT)
+puts f(N, X)
+
+# puts "count:#{$count}"
